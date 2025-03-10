c90e6a8e173be865de0ab58237e2d61bf1cf0ccf
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-03-10 22:47:20 +0700

    menambahkan implementasi dengan state-based construction

--- tpmod4/Program.cs
+++ tpmod4/Program.cs
@@ -29,3 +29,90 @@ public class Kodepos
         return "kode pos tidak ditemukan";
     }
 }
+
+public class DoorMachine
+{
+    enum State { terkunci, terbuka, keluar };
+    public void door()
+    {
+
+        State state = State.terkunci;
+        String[] status = { "terkunci", "terbuka", "keluar" };
+
+        while (state != State.keluar)
+        {
+
+            Console.WriteLine("\n" + "Pintu " + status[(int)state] + "\n");
+            Console.WriteLine("Masukkan perintah (masukkan keluar untuk kembali) : ");
+            String command = Console.ReadLine();
+
+            switch (state)
+            {
+                case State.terkunci:
+                    if (command == "terbuka")
+                    {
+                        state = State.terbuka;
+                    }
+                    else if (command == "keluar")
+                    {
+                        Program.Main();
+                    }
+                    break;
+                case State.terbuka:
+                    if (command == "terkunci")
+                    {
+                        state = State.terkunci;
+                    }
+                    else if (command == "keluar")
+                    {
+                        Program.Main();
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("1. program kode pos");
+        Console.WriteLine("2. program Door Machine");
+        Console.WriteLine("0. exit");
+        Console.WriteLine("nasukkan pilihan : ");
+
+        String masuk = Console.ReadLine();
+
+        while (masuk != "0")
+        {
+            if (masuk == "1")
+            {
+                Kodepos Kode = new Kodepos();
+                Console.WriteLine("masukkan nama kelurahan (kembali untuk ke menu utama) : ");
+                string kelurahan = Console.ReadLine() ?? "";
+
+                if (kelurahan != "kembali")
+                {
+                    string kode = Kodepos.getKodePos(kelurahan);
+                    Console.WriteLine($"kode pos {kelurahan} : {kode}");
+                }
+                else
+                {
+                    Main();
+                }
+
+            }
+            else if (masuk == "2")
+            {
+                DoorMachine door = new DoorMachine();
+                door.door();
+            }
+            else if (masuk == "0")
+            {
+                break;
+            }
+        }
+    }
+}
